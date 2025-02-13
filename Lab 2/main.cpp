0fa6f7a3fab6ce88676565d24bdf1b13613999bf
Artem <artemvip827@gmail.com>
2025-02-13 13:12:54 +0200

    Create main.cpp

---
+++ Lab 2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include "ClassLib.h"
+using namespace std;
+
+int main() 
+{
+    double x1, y1, x2, y2;
+
+    cout << "Enter x1: ";
+    cin >> x1;
+    cout << "Enter y1: ";
+    cin >> y1;
+    cout << "Enter x2: ";
+    cin >> x2;
+    cout << "Enter y2: ";
+    cin >> y2;
+
+    LineSegment segment(x1, x2, y1, y2);
+
+    cout << "--------------------------------" << endl;
+    cout << "X1: " << x1 << " Y1: " << y1 << endl;
+    cout << "X2: " << x2 << " Y2: " << y2 << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Segment length is: " << segment.length() << endl;
+    cout << "Angle with OX is: " << segment.angleOX() << endl;
+    cout << "--------------------------------" << endl;
+
+    return 0;
+}
