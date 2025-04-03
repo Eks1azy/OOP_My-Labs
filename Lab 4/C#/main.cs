b5ff406f3d210cd40f5c3dd6ce13114a17778ccf
Artem <artemvip827@gmail.com>
2025-04-03 14:11:11 +0300

    Update main.cs

--- Lab 4/C#/main.cs
+++ Lab 4/C#/main.cs
@@ -1,1 +1,16 @@
+using System;
+using Artem;
 
+class _Main
+{
+    static void Main(string[] args)
+    {
+        LetterString LetterString = new LetterString("dcgfba");
+        Console.WriteLine($"Похідний рядок: {LetterString.GetValue()}");
+
+        LetterString.Sort_Str();
+
+        Console.WriteLine($"Відсортований рядок: {LetterString.GetValue()}");
+        Console.WriteLine($"Довжина рядка: {LetterString.GetLength()}");
+    }
+}
