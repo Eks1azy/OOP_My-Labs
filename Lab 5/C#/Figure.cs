223dd0c0d0a4a4156cc8838fc3875438a403d684
Artem <artemvip827@gmail.com>
2025-04-16 12:48:42 +0300

    Create Figure.cs

---
+++ Lab 5/C#/Figure.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Artem_Figure
+{
+    // Абстрактний базовий клас
+    public abstract class Figure
+    {
+        public abstract double Area();     // Площа
+        public abstract double Perimeter();  // Довжина
+
+       
+    }
+}
